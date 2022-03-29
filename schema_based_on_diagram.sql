429642ed8292d69b567ff092c2878abf8b377734
jihaneh <jijih@hotmail.fr>
2022-03-29 11:31:56 +0200

    Add patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,8 @@
 /* Database schema to keep the structure of entire database. */
+
+CREATE TABLE patients(
+	id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+	date_of_birth DATE
+);
+
