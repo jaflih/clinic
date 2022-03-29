ab8ad9de929baa7112f2ae9eec3df042dab2597d
ginohmk <michotall95@gmail.com>
2022-03-29 10:43:51 +0100

    Add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,5 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE Clinic;
 
 CREATE TABLE patients(
 	id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
@@ -6,3 +7,10 @@ CREATE TABLE patients(
 	date_of_birth DATE
 );
 
+CREATE TABLE medical_histories (
+id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+admitted_at TIMESTAMP,
+patient_id INT,
+status VARCHAR(255));
+
+ALTER TABLE medical_histories ADD CONSTRAINT id_fk FOREIGN KEY (patient_id) REFERENCES patients(id);
