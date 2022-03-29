1308e6c5449ce1a232fba0c277bd2c9e81b3d26c
jihaneh <jijih@hotmail.fr>
2022-03-29 11:52:56 +0200

    Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,16 @@ patient_id INT,
 status VARCHAR(255));
 
 ALTER TABLE medical_histories ADD CONSTRAINT id_fk FOREIGN KEY (patient_id) REFERENCES patients(id);
+
+CREATE TABLE invoices(
+	id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+	generated_at TIMESTAMP,
+	payed_at TIMESTAMP,
+	medical_history_id int
+);
+
+ALTER TABLE invoices 
+ADD CONSTRAINT medical_histories_constraint 
+FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id);
+
